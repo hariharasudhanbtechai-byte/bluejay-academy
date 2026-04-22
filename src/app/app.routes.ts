51b8b4d338067ef2aaaa69d62729d7676f87efa4
Harihara Sudhan A <hariharasudhan.btechai@gmail.com>
2026-04-22 14:53:45 +0530

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { About } from './about/about';
+import { Teachers } from './teachers/teachers';
+import { Activities } from './activities/activities';
+import { Safety } from './safety/safety';
+import { Admissions } from './admissions/admissions';
+import { Contact } from './contact/contact';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: Home },
+  { path: 'about', component: About },
+  { path: 'teachers', component: Teachers },
+  { path: 'activities', component: Activities },
+  { path: 'safety', component: Safety },
+  { path: 'admissions', component: Admissions },
+  { path: 'contact', component: Contact },
+];
---
+++ src/app/safety/safety.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-safety',
+  imports: [],
+  templateUrl: './safety.html',
+  styleUrl: './safety.css',
+})
+export class Safety {}
